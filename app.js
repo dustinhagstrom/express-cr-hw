f4a06071a73ec65229cd370f8f4de719266be19f
dustnhgstrm <77700279+dustnhgstrm@users.noreply.github.com>
2021-05-20 14:50:17 -0400

    added a module.exports to bottom of app.js and commented out the listen method to allow hw testing.

--- app.js
+++ app.js
@@ -18,6 +18,8 @@ app.use(express.json());
 app.use("/", indexRouter);
 app.use("/api/todo", todoRouter);
 
-app.listen(3000, function () {
-  console.log(`The server is running on port ${3000}`);
-});
+// app.listen(3000, function () {
+//   console.log(`The server is running on port ${3000}`);
+// });
+
+module.exports = app;
